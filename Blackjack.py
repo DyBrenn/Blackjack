bcf89719150a94e440db489848b257be1c7220be
Dylan <dylan.brennan@uconn.edu>
2017-01-31 21:52:19 -0500

    Blackjack_final

--- Blackjack.py
+++ Blackjack.py
@@ -1,20 +1,23 @@
-#to-do: finish fixing bets and write comments
 import random
 class deck:
     def __init__(self, funds = None):
+        #initiallize deck class
         self._funds = funds if funds is not None else 0
         self._bet = 0
         self._cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
         self._suits = ['Diamond', 'Spade', 'Club', 'Heart']
         self._deck = []
         self._total = 0
+        #creates a full deck of cards
         for i in self._cards:
             for j in self._suits:
                 self._deck.append((j,i))
         self._hand = []
         self._dealer = []
+        self._dealertot = 0
 
     def deal(self):
+        #Always deals a random card from a full deck
         num = random.randint(0,51)
         card = self._deck[num]
         return card
@@ -30,33 +33,45 @@ class deck:
 
 class blackjack(deck):
     def __init__(self, funds = None):
-        print('Use .bet() to bet money from your funds (bet will remain same if you don\'t change it in between rounds), use .start() to start a game, .hit() to be given a card (always draws from a full deck), .hold() to hold onto your cards, .double() to double down (only do this after .start (2 cards) and you are given only 1 card and can no longer hit). To add funds use .changefunds(amount) and use .showfunds() to see your funds. Dealer must hit on 16. Enjoy!')
+        print('Use bj.bet() to bet money from your funds (bet will remain same if you don\'t change it in between rounds), use bj.start() to start a new game, bj.hit() to be given a card (always draws from a full deck), bj.hold() to hold onto your cards, bj.double() to double down (can only do this directly after bj.start (only have 2 cards). Can\'t hit after doubling down). To add funds use bj.changefunds(amount) and use bj.showfunds() to see your funds. Dealer must hit on 16. Enjoy and may the odds be ever random!')
         deck.__init__(self, funds)
+        #Initialize blackjack class
         self._start = False
         #Tracks if ace has been used as 11 so if bust it can be changed to a 1 for both dealer and player
         self._ace = 0
         self._ace2 = 0
+        #Tracks if player has doubled down
+        self._double = False
         
     def bet(self, amount):
+        #Bets constrained by 0<bet<=self._funds
         if amount > 0 and self._funds >= amount:
             self._bet = amount
         else:
             print("Please enter a positive amount to bet")
 
     def hit(self):
+        #Ensures bj.start() was used to begin new game
         if self._start == True:
+            initial = self._total
             i = self.deal()
             self._hand.append(i)
+            #Handles face cards
             if i[1] in ['K', 'Q', 'J']:
+                #Track hand value with self._total
                 self._total += 10
+            #Handles aces
             if i[1] == 'A':
                 if self._total < 11:
                     self._total += 11
                     self._ace += 1
                 else:
                     self._total += 1
+            #Catches any non-face cards
             if isinstance(i[1], int):
                 self._total += i[1]
+            if initial == self._total:
+                self._total += 10
             if self._total > 21:
                 if self._ace > 0:
                     self._ace -= 1
@@ -65,60 +80,71 @@ class blackjack(deck):
                     self._funds -= self._bet
                     print("Oops, you busted. hand:",self._hand ,"total:", self._total)
                     self._start = False
-                    self._total = 0
-                    self._hand = []
-                    self._dealer = []
             if self._total == 21:
-                print("Blackjack! 2x winnings! hand:", self._hand, "total:", self._total)
-                self._funds += (2*self._bet)
+                print("Blackjack! You win! hand:", self._hand, "total:", self._total)
+                self._funds += self._bet
                 self._start = False
-                self._total = 0
-                self._hand = []
-                self._dealer = []
             if self._start == True:
                 print('Hand:', self._hand, ', total:', self._total)
         else:
             print("Please use .start() to begin a game.")
     def hold(self):
         if self._start == True:
-            dealer = self._dealer[1]
-            while dealer < 17:
+            #dealer keeps track of dealer hand value
+            self._dealertot += self._dealer[1]
+            while self._dealertot < 17:
                 num = self.deal()[1]
                 if num in ['K', 'Q', 'J']:
-                    dealer += 10
+                    self._dealertot += 10
                 if num == 'A':
-                    if dealer < 11:
-                        dealer += 11
+                    if self._dealertot < 11:
+                        self._dealertot += 11
                         self._ace2 += 1
                     else:
-                        dealer += 1
+                        self._dealertot += 1
                 if isinstance(num, int):
-                    dealer += num
-            if dealer > 21:
+                    self._dealertot += num
+            #Handle who wins
+            if self._dealertot > 21:
                 if self._ace2 > 0:
                     self._ace2 -= 1
-                    dealer -= 10
+                    self._dealertot -= 10
+                    self.hold()
                 else:
-                    print("You win, dealer busted! you:", self._total,'dealer:', dealer)
-                    self._funds += self._bet
-                    self._bet = 0
+                    print("You win, dealer busted! you:", self._total,'dealer:', self._dealertot)
+                    if self._double:
+                        self._funds += (2*self._bet)
+                    else:
+                        self._funds += self._bet
             else:
-                if dealer > self._total:
+                if self._dealertot > self._total:
                     self._funds -= self._bet
-                    print("Sorry, dealer's got you beat. you:", self._total,'dealer:', dealer)
-                if dealer == self._total:
-                    print("You push. you:", self._total,'dealer:', dealer)
-                if self._total > dealer:
-                    print("Winner, winner, chicken dinner! you:", self._total,'dealer:', dealer)
-                    self._funds += self._bet
+                    print("Sorry, dealer's got you beat. you:", self._total,'dealer:', self._dealertot)
+                    if self._double:
+                    	self._funds -= 2*self._bet
+                    else:
+                    	self._funds -= self._bet
+                if self._dealertot == self._total:
+                    print("You push. you:", self._total,'dealer:', self._dealertot)
+                if self._total > self._dealertot:
+                    print("Winner, winner, chicken dinner! you:", self._total,'dealer:', self._dealertot)
+                    if self._double:
+                    	self._funds += 2*self._bet
+                    else:
+                    	self._funds += self._bet
             self._start = False
-            self._total = 0
-            self._hand = []
-            self._dealer = []
         else:
             print("Please use .start() to begin a game.")
     def start(self):
+        #Initialize all variables clean states
         self._start = True
+        self._total = 0
+        self._hand = []
+        self._dealer = []
+        self._double = False
+        self._dealertot = 0
+        self._ace = 0
+        self._ace2 = 0
         self._hand.append(self.deal())
         self._hand.append(self.deal())
         self._dealer.append(self.deal())
@@ -145,14 +171,15 @@ class blackjack(deck):
         if self._total == 21:
             print("Blackjack! 2x winnings! hand:", self._hand,"total:", self._total)
             self._start = False
-            self._total = 0
-            self._hand = []
-            self._dealer = []
+            self._funds += 2*self._bet
         else:
             print("Your hand is:", self._hand,'total:', self._total, '\n' 'dealers card:', self._dealer[0])
 
     def double(self):
+        #Double your bet and draw once card
         if self._start == True and len(self._hand) == 2:
+            #Used to pay or take double
+            self._double = True
             card = self.deal()
             self._hand.append(card)
             if card[1] in ['K', 'Q', 'J']:
@@ -166,9 +193,11 @@ class blackjack(deck):
                 self._total += card[1]
             if self._total > 21:
                 print("Whoops, looks like you busted. total:", self._total)
+                self._funds -= 2*self._bet
                 self._start = False
-                self._total = 0
-                self._hand = []
-                self._dealer = []
             else:
                 self.hold()
+
+if __name__ == "__main__":
+	cash = input("How much money would you like to start with? ")
+	bj = blackjack(int(cash))
